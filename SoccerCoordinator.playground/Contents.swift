43116b9066e66047a1a47d8114232e334255bf82
zswenjin <zswenjin.li@gmail.com>
2017-08-26 16:29:21 +1000

    add Dictionary for players (Part 1)

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,8 +1,24 @@
-//: Playground - noun: a place where people can play
+/*
+ ** Part 1: ** We have provided information for the 18 players in the Player Info spreadsheet. Please choose an appropriate data type to store the information for each player. In addition, create an empty collection variable to hold all the players’ data. Once you have decided on what tools to use, manually enter the player data so it can be used in Part 2.
+*/
 
-import UIKit
-
-var str = "Hello, playground"
-
-//hello "Louis" 
-//yoyo check it out
+var playersData: [[String: Any]] = [
+    ["Name": "Joe Smith", "Height": 42, "SoccerExperience": true, "GuardianName": "Jim and Jan Smith"],
+    ["Name": "Jill Tanner", "Height": 36, "SoccerExperience": true, "GuardianName": "Clara Tanner"],
+    ["Name": "Bill Bon", "Height": 43, "SoccerExperience": true, "GuardianName": "Sara and Jenny Bon"],
+    ["Name": "Eva Gordon", "Height": 45, "SoccerExperience": false, "GuardianName": "Wendy and Mike Gordon"],
+    ["Name": "Matt Gill", "Height": 40 , "SoccerExperience": false, "GuardianName": "Charles and Sylvia Gill"],
+    ["Name": "Kimmy Stein", "Height": 41, "SoccerExperience": false, "GuardianName": "Bill and Hillary Stein"],
+    ["Name": "Sammy Adams", "Height": 45, "SoccerExperience": false, "GuardianName": "Jeff Adams"],
+    ["Name": "Karl Saygan", "Height": 42, "SoccerExperience": true, "GuardianName": "Heather Biedsoe"],
+    ["Name": "Suzane Greenberg", "Height": 44, "SoccerExperience": true, "GuardianName": "Henrietta Dumas"],
+    ["Name": "Sal Dali", "Height": 41, "SoccerExperience": false, "GuardianName": "Gala Dali"],
+    ["Name": "Joe Kavalier", "Height": 39, "SoccerExperience": false, "GuardianName": "Sam and Elaine Kavalier"],
+    ["Name": "Ben Finkelstein", "Height": 44, "SoccerExperience": false, "GuardianName": "Aaron and Jill Finkelstein"],
+    ["Name": "Diego Soto", "Height": 41, "SoccerExperience": true, "GuardianName": "Robin and Sarika Soto"],
+    ["Name": "Chloe Alaska", "Height": 47, "SoccerExperience": false, "GuardianName": "David and Jamie Alaska"],
+    ["Name": "Arnold Willis", "Height": 43, "SoccerExperience": false, "GuardianName": "Claire Willis"],
+    ["Name": "Phillip Helm", "Height": 44, "SoccerExperience": true, "GuardianName": "Thomas Helm and Eva Jones"],
+    ["Name": "Les Clay", "Height": 42, "SoccerExperience": true, "GuardianName": "Wynonna Brown"],
+    ["Name": "Herschel Krustofski", "Height": 45, "SoccerExperience": true, "GuardianName": "Hyman and Rachel Krustofski"]
+    ]
