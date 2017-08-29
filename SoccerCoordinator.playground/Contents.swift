00400a7f31f98d19133a2df58e6d96855d6ab0dd
zswenjin <zswenjin.li@gmail.com>
2017-08-29 21:11:35 +1000

    bug fixes

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,25 +1,8 @@
 /*
- ** Part 1: ** We have provided information for the 18 players in the Player Info spreadsheet. Please choose an appropriate data type to store the information for each player. In addition, create an empty collection variable to hold all the players’ data. Once you have decided on what tools to use, manually enter the player data so it can be used in Part 2.
- 
- ** Part 2: ** Create logic to iterate through all 18 players and assign them to teams such that the number of experienced players on each team are the same. Store each team’s players in its own new collection variable for use in Part 3. (Please note: your logic should work correctly regardless of the initial ordering of the players and should work, if we theoretically had more or less than 18 players, so NO MAGIC NUMBERS!)
- 
- Also, if you would like to attain an “exceeds expectations” rating for this project, add logic to ensure that each team's average height is within 1.5 inches of the others.
- 
- ** Part 3: ** Create logic that iterates through all three teams of players and generates a personalized letter to the guardians, letting them know which team the child has been placed on and when they should attend their first team team practice. As long as you provide the necessary information (player name, team name, guardian names, practice date/time), feel free to have fun with the content of the letter. The team practice dates/times are as follows:
- 
- Dragons - March 17, 1pm
- Sharks - March 17, 3pm
- Raptors - March 18, 1pm
- When your complete code is run in a playground the letters should be stored in a collection variable named 'letters'. When the code is run, the letters should be visible in the console.
- 
- You can reference the resources provided in the Project Resources area for additional information on how to implement and use the Swift Collections.
- 
- As always, meaningful and concise code comments are expected. Your code should be written and refined in an Xcode playground. Be sure to upload it to GitHub, as per the instructions provided in the Soccer Coordinator Video Instruction (see the link below).
- 
- One note regarding the usage of GitHub. Since it is a version control tool, and it is best to do an incremental check in when you completed a feature, fix, or enhancement. Also when adding comment to each check-in, a good practice is to add comments for the specific things that's changed, e.g. "added logic for height based assignment", "fixed duplicate printing of dragon team". This way you can go back and know exactly where certain enhancements/bugs might be introduced.
+                     --------------------------------------------------Part 1--------------------------------------------------
+We have provided information for the 18 players in the Player Info spreadsheet. Please choose an appropriate data type to store the information for each player. In addition, create an empty collection variable to hold all the players’ data. Once you have decided on what tools to use, manually enter the player data so it can be used in Part 2.
+                     --------------------------------------------------Part 1--------------------------------------------------
 */
-
-
 //information for each player
 let player1: [String: Any] = [
     "name": "Joe Smith",
@@ -171,6 +154,14 @@ var players = [
     player17,
     player18]
 
+/*
+ --------------------------------------------------Part 2--------------------------------------------------
+ Create logic to iterate through all 18 players and assign them to teams such that the number of experienced players on each team are the same. Store each team’s players in its own new collection variable for use in Part 3. (Please note: your logic should work correctly regardless of the initial ordering of the players and should work, if we theoretically had more or less than 18 players, so NO MAGIC NUMBERS!)
+ 
+ Also, if you would like to attain an “exceeds expectations” rating for this project, add logic to ensure that each team's average height is within 1.5 inches of the others.
+ --------------------------------------------------Part 2--------------------------------------------------
+ */
+
 //Three group: Dragons. Sharks, Raptors
 var dragonsTeam: [[String:Any]] = []
 var sharksTeam: [[String:Any]] = []
@@ -189,14 +180,16 @@ for player in players {
     }
 }
 
-//group experience players and no experience in each team with evenly numbers of experience/ no experience players
+//group experience players and no experience in each team with evenly numbers of experience/no experience players
 for expPlayers in hasExperience {
-if sharksTeam.count <= dragonsTeam.count && sharksTeam.count <= raptorsTeam.count {
+    if sharksTeam.count <= dragonsTeam.count && sharksTeam.count <= raptorsTeam.count {
     sharksTeam.append(expPlayers)
     } else if dragonsTeam.count <= sharksTeam.count && dragonsTeam.count <= raptorsTeam.count {
         dragonsTeam.append(expPlayers)
-    } else {
+    } else if raptorsTeam.count <= dragonsTeam.count && raptorsTeam.count <= sharksTeam.count{
         raptorsTeam.append(expPlayers)
+    } else {
+        print("error")
     }
 }
 
@@ -205,14 +198,16 @@ for noExpPlayers in noExperience {
         sharksTeam.append(noExpPlayers)
     } else if dragonsTeam.count <= sharksTeam.count && dragonsTeam.count <= raptorsTeam.count {
         dragonsTeam.append(noExpPlayers)
-    } else {
+    } else if raptorsTeam.count <= dragonsTeam.count && raptorsTeam.count <= sharksTeam.count {
         raptorsTeam.append(noExpPlayers)
+    } else {
+        print("error")
     }
 }
 
 
 /* 
--------------------------------------------------result for each team--------------------------------------------------------
+             -------------------------------------------------result for each team--------------------------------------------------------
 print(dragonsTeam)
 [["name": "Jill Tanner", "height": 36.0, "hasExperience": true, "guardian": "Clara Tanner"], 
  ["name": "Suzanne Greenberg", "height": 44.0, "hasExperience": true, "guardian": "Henrietta Dumas"], 
@@ -241,9 +236,52 @@ print(raptorsTeam)
   ["name": "Joe Kavalier", "height": 39.0, "hasExperience": false, "guardian": "Sam and Elaine Kavalier"], 
   ["name": "Arnold Willis", "height": 43.0, "hasExperience": false, "guardian": "Claire Willis"]]
  
-raptorsTeam.count //6 
--------------------------------------------------result for each team--------------------------------------------------------
+raptorsTeam.count //6
+             -------------------------------------------------result for each team--------------------------------------------------------
 */
+
+/*
+                     --------------------------------------------------Part 3--------------------------------------------------
+ Create logic that iterates through all three teams of players and generates a personalized letter to the guardians, letting them know which team the child has been placed on and when they should attend their first team team practice. As long as you provide the necessary information (player name, team name, guardian names, practice date/time), feel free to have fun with the content of the letter. The team practice dates/times are as follows:
+ 
+ Dragons - March 17, 1pm
+ Sharks - March 17, 3pm
+ Raptors - March 18, 1pm
+ When your complete code is run in a playground the letters should be stored in a collection variable named 'letters'. When the code is run, the letters should be visible in the console.
+ 
+ You can reference the resources provided in the Project Resources area for additional information on how to implement and use the Swift Collections.
+ 
+ As always, meaningful and concise code comments are expected. Your code should be written and refined in an Xcode playground. Be sure to upload it to GitHub, as per the instructions provided in the Soccer Coordinator Video Instruction (see the link below).
+ 
+ One note regarding the usage of GitHub. Since it is a version control tool, and it is best to do an incremental check in when you completed a feature, fix, or enhancement. Also when adding comment to each check-in, a good practice is to add comments for the specific things that's changed, e.g. "added logic for height based assignment", "fixed duplicate printing of dragon team". This way you can go back and know exactly where certain enhancements/bugs might be introduced.
+
+                     --------------------------------------------------Part 3--------------------------------------------------
+ */
+
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
